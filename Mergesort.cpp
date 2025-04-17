b742328189d75952d8d6e2693ea950ce326d98fe
Rouf1224 <ahmadroufr24@gmail.com>
2025-04-17 17:43:33 +0700

    Mergesort 2

--- Mergesort.cpp
+++ Mergesort.cpp
@@ -3,3 +3,12 @@ using namespace std;
 
 int arr[20], B[20]; // Array utama dan array bantu
 int n;
+
+void input()
+{
+    while(true)
+    {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
+    }
+}
